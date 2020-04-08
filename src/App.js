137376ac27000be2a798ba118ec9c922f2ae2b9a
cara <xw33@outlook.com>
2020-04-08 20:03:40 +0100

    finish mvp

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import './index.css'
-// import './App.css';
+import { useState } from 'react';
+import produce from 'immer';
+import './index.css';
+
 
 function App() {
+
+  const [data, setData] = useState('');
+
+  const handleChange = (e) => {
+    const data = e.target.data
+    setData(data)  
+  };
+
+ 
+
+
   return (
     <div id="paper">
-    <div id="pattern">
-      <div id="content">
-        
+      <div id="pattern">
+        <div id="content">
+          <textarea 
+            value={data} 
+            onChange={handleChange}
+            className="textbox" 
+            />        
+           
+          </div>
       </div>
-    </div>
 
-  </div>
+    </div>
   );
 }
 
